275664ce7050906cbb5a059ebf1254e2c1e227e7
spmvg <13852721+spmvg@users.noreply.github.com>
2026-08-16 19:55:44 +0200

    script [feature/run-on-rpi5]

--- scripts/dev-setup-run.sh
+++ scripts/dev-setup-run.sh
@@ -1,6 +1,12 @@
 # This script assumes that:
 #   - The location of the repository is `~/backlooper_backend`
 #   - The virtual environment is located at `~/backlooper_backend/.venv`
+#   - The input and output devices are both at index 0
+
+export INPUT_DEVICE_ID=0
+export OUTPUT_DEVICE_ID=0
+
+ulimit -n 1048576  # workaround for https://github.com/spmvg/backlooper_backend/issues/3
 
 cd ~/backlooper_backend
 source .venv/bin/activate
