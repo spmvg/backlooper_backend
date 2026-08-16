d400709b6a2fcb67f8381c065cf678244f9763ac
spmvg <13852721+spmvg@users.noreply.github.com>
2026-08-16 20:06:24 +0200

    TODO [feature/run-on-rpi5]

--- scripts/dev-setup-run.sh
+++ scripts/dev-setup-run.sh
@@ -2,9 +2,11 @@
 #   - The location of the repository is `~/backlooper_backend`
 #   - The virtual environment is located at `~/backlooper_backend/.venv`
 #   - The input and output devices are both at index 0
+#   - TODO: MIDI
 
 export INPUT_DEVICE_ID=0
 export OUTPUT_DEVICE_ID=0
+# TODO: MIDI
 
 ulimit -n 1048576  # workaround for https://github.com/spmvg/backlooper_backend/issues/3
 
