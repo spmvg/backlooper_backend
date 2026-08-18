c3204b2a5bfe9c56f627244a3df2ca37a4ea6d08
spmvg <13852721+spmvg@users.noreply.github.com>
2026-08-18 17:01:45 +0200

    continue [feature/run-on-rpi5]

--- scripts/dev-setup-run.sh
+++ scripts/dev-setup-run.sh
@@ -12,7 +12,7 @@ export OUTPUT_DEVICE_ID=0
 ulimit -n 1048576  # workaround for https://github.com/spmvg/backlooper_backend/issues/3
 
 cd ~/backlooper_backend
-git pull
+timeout 10 git pull || echo "git pull failed, continuing with local version"
 source .venv/bin/activate
 python -m pip install -e .
 python -m backlooper
